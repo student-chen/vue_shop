2bcc6859cd359d540872a07ade901a2a754b0c34
student-chen <3196614820@qq.com>
2020-03-11 15:04:26 +0800

    提交原始代码

---
+++ src/views/Login.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="login">
+        <el-button type="submit">登录页面</el-button>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
